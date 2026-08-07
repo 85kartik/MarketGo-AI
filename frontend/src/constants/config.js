5e3c6306b6239607b450fdb90eb5b9e5dbe604b6
85kartik <kartikparab887@email.com>
2026-08-07 23:35:00 +0530

    Update frontend and server

--- frontend/src/constants/config.js
+++ frontend/src/constants/config.js
@@ -1,5 +1,5 @@
-export const BASE_URL = "http://192.168.0.100:5252/api/v1";
-export const SERVER_ORIGIN = "http://192.168.0.100:5252";
+export const BASE_URL = "http://192.168.0.102:5252/api/v1";
+export const SERVER_ORIGIN = "http://192.168.0.102:5252";
 
 export const TOKEN_KEY = "marketgo_token";
 export const APP_NAME = "MarketGo";
